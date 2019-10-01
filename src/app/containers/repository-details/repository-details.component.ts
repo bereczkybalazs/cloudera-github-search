6435be85d27f392f2c06961967bf7f39e7a69b52
bereczkybalazs <bereczkybalazs1@gmail.com>
2019-10-01 15:03:51 +0200

    issues list

--- src/app/containers/repository-details/repository-details.component.ts
+++ src/app/containers/repository-details/repository-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {log} from 'util';
+import {Issue} from '../../models/Issue';
+import {GithubIssueSearchService} from '../../services/github-issue-search/github-issue-search.service';
+import {IssueResponse} from '../../models/IssueResponse';
 
 @Component({
   selector: 'app-repository-details',
@@ -11,12 +13,20 @@ export class RepositoryDetailsComponent implements OnInit {
 
   owner: string;
   project: string;
+  issues: Array<Issue>;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private issueSearch: GithubIssueSearchService
+  ) { }
 
   ngOnInit() {
     this.owner = this.route.snapshot.paramMap.get('owner');
     this.project = this.route.snapshot.paramMap.get('project');
+    const searchQuery = `${this.owner}/${this.project}`;
+    this.issueSearch.searchByQuery(searchQuery).subscribe((res: IssueResponse) => {
+      this.issues = res.items;
+    });
   }
 
 }
